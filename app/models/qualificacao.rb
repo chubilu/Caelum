6204008d2b73a309be9a79540d563cd714772a02
Rodrigo Velasquez <rodrigo.velasquez@sentrylab.com>
2014-10-14 13:51:11 -0300

    End cap 7 ActiveRecord

--- app/models/qualificacao.rb
+++ app/models/qualificacao.rb
@@ -6,6 +6,10 @@ class Qualificacao < ActiveRecord::Base
                                    message: " - deve ser um número entre 0 e 10"
   validates_numericality_of :valor_gasto, greater_than: 0,
                                           message: " - deve ser um número maior que 0"
+  validates_presence_of :cliente_id, :restaurante_id
+  validates_associated :cliente, :restaurante
+
   belongs_to :cliente
   belongs_to :restaurante
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014154425) do
+ActiveRecord::Schema.define(version: 20141014162322) do
 
   create_table "clientes", force: true do |t|
     t.string   "nome",       limit: 80
@@ -26,11 +26,18 @@ ActiveRecord::Schema.define(version: 20141014154425) do
     t.datetime "updated_at"
   end
 
+  create_table "pratos_restaurantes", id: false, force: true do |t|
+    t.integer "prato_id"
+    t.integer "restaurante_id"
+  end
+
   create_table "qualificacoes", force: true do |t|
     t.float    "nota"
     t.float    "valor_gasto"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "cliente_id"
+    t.integer  "restaurante_id"
   end
 
   create_table "receitas", force: true do |t|
